04617060e085274ddeccff8a7232c35fb092513f
Gabriel Cervante <andersongabriel7377@gmail.com>
2021-01-20 03:46:36 -0300

    little modification in getjsfilesfromaurl

--- Get-Js-Files-From-a-Url/getjsfiles.sh
+++ Get-Js-Files-From-a-Url/getjsfiles.sh
@@ -2,4 +2,6 @@
 
 curl $1 -o page.html
 
-find . -type f -name page.html | html-tool attribs src | grep '\.js' | tee -a $2 
+find . -type f -name page.html | html-tool attribs src | grep '\.js' | tee -a $2
+
+rm page.html
